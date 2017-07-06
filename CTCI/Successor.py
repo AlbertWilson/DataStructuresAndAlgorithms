a820b08ca2553255f1bff8daf158e85d4c6b665f
Albert Wilson <avw5331@gmail.com>
2017-07-05 19:04:16 -0700

    Update Successor.py

--- CTCI/Successor.py
+++ CTCI/Successor.py
@@ -26,34 +26,4 @@ def inOrderList(root, arr):
         inOrderList(root.right, arr)
     return arr
 
-node1 = Node()
-node1.val = 1
-node2 = Node()
-node2.val = 2
-node3 = Node()
-node3.val = 3
-node4 = Node()
-node4.val = 4
-node5 = Node()
-node5.val = 5
-node6 = Node()
-node6.val = 6
-node7 = Node()
-node7.val = 7
-node8 = Node()
-node8.val = 8
-node9 = Node()
-node9.val = 9
-node10 = Node()
-node10.val = 10
 
-node5.left = node2
-node5.right = node7
-
-node2.left = node1
-node2.right = node3
-
-node7.left = node6
-node7.right = node8
-
-print successor(node5, node8)
