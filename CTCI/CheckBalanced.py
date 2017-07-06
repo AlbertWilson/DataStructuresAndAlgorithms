1f5f94255c384ee55d70455f211fd176909ae21e
Albert Wilson <avw5331@gmail.com>
2017-07-05 19:03:22 -0700

    Update CheckBalanced.py

--- CTCI/CheckBalanced.py
+++ CTCI/CheckBalanced.py
@@ -29,32 +29,3 @@ def checkBalanced(root):
 
     return False
 
-
-node1 = Node()
-node1.val = 1
-node2 = Node()
-node2.val = 2
-node3 = Node()
-node3.val = 3
-node4 = Node()
-node4.val = 4
-node5 = Node()
-node5.val = 5
-node6 = Node()
-node6.val = 6
-node7 = Node()
-node7.val = 7
-node8 = Node()
-node8.val = 8
-node9 = Node()
-node9.val = 9
-node10 = Node()
-node10.val = 10
-
-node1.left = node2
-node1.right = node3
-
-node2.left = node4
-node4.left = node10
-
-print checkBalanced(node1)
